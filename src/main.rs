634b9a26b9b6a2f57142d47849a404a08d185555
Felix Chapman <aelred717@gmail.com>
2018-12-17 12:42:02 +0000

    Allow human to play as white

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,40 @@ use chess::search;
 use chess::Board;
 use chess::Move;
 use chess::Player;
+use std::collections::HashSet;
+use std::io;
+use std::io::BufRead;
+use std::io::Write;
 
 fn main() {
+    let stdin = io::stdin();
+    let mut input = stdin.lock().lines();
+
     let mut board = Board::default();
     let mut searcher = search::Searcher::default();
     println!("{}", board);
 
-    while let Some(mov) = decide(&mut searcher, &board) {
-        board.make_move(mov);
+    loop {
+        let valid_moves: HashSet<Move> = board.moves().collect();
+
+        let player_move = loop {
+            print!("Player: ");
+            io::stdout().flush().expect("Could not flush stdout");
+
+            let line = input.next().unwrap().unwrap();
+            if let Ok(mov) = line.parse() {
+                if valid_moves.contains(&mov) {
+                    break mov;
+                }
+            }
+        };
+
+        board.make_move(player_move);
+        println!("\n{}", board);
+
+        let computer_move = decide(&mut searcher, &board).unwrap();
+        println!("Computer: {}", computer_move);
+        board.make_move(computer_move);
         println!("\n{}", board);
     }
 }
