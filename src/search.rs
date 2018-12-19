4daa6ea0c65f25d11cd2cfec58b73902cefe4b29
Felix Chapman <aelred717@gmail.com>
2018-12-19 18:26:47 +0000

    Fix issue with overflow

--- src/search.rs
+++ src/search.rs
@@ -5,6 +5,9 @@ use enum_map::EnumMap;
 use rand::seq::SliceRandom;
 use std::collections::HashMap;
 
+const HIGH_SCORE: i32 = std::i32::MAX;
+const LOW_SCORE: i32 = -HIGH_SCORE;
+
 type Key = (
     EnumMap<crate::Player, EnumMap<crate::PieceType, crate::Bitboard>>,
     Player,
@@ -35,7 +38,7 @@ trait AlphaBetaSearcher {
     fn should_terminate(board: &mut Board) -> bool;
 
     fn run(&mut self, board: &mut Board, depth: u32) -> i32 {
-        self.search(board, depth, std::i32::MIN, std::i32::MAX)
+        self.search(board, depth, LOW_SCORE, HIGH_SCORE)
     }
 
     fn search(&mut self, board: &mut Board, depth: u32, alpha: i32, beta: i32) -> i32 {
@@ -54,72 +57,59 @@ trait AlphaBetaSearcher {
         let mut moves = board.moves().peekable();
 
         if moves.peek().is_none() {
-            return std::i32::MIN;
+            return LOW_SCORE;
         }
 
         if depth == 0 {
             return self.evaluate_leaf(board);
         }
 
-        let mut best_value = std::i32::MIN;
-
         for mov in moves {
             board.make_move(mov);
             let value = -self.search(board, depth - 1, -beta, -alpha);
             board.unmake_move(mov);
 
-            if value > best_value {
-                best_value = value;
-
-                alpha = i32::max(alpha, best_value);
-
-                if alpha >= beta {
-                    break;
-                }
+            if value >= beta {
+                return beta;
+            }
+            if value > alpha {
+                alpha = value;
             }
         }
 
-        best_value
+        alpha
     }
 }
 
 impl Searcher {
-    pub fn run(&mut self, board: &mut Board) -> (Option<Move>, i32) {
-        let mut alpha = std::i32::MIN;
-        let beta = std::i32::MAX;
-        let depth = 5;
+    const DEPTH: u32 = 4;
 
+    pub fn run(&mut self, board: &mut Board) -> (Option<Move>, i32) {
         let mut moves = board.moves().peekable();
 
         if moves.peek().is_none() {
-            return (None, std::i32::MIN);
+            return (None, LOW_SCORE);
         }
 
+        let mut alpha = LOW_SCORE;
         let mut best_moves = vec![];
-        let mut best_value = std::i32::MIN;
 
         for mov in moves {
             board.make_move(mov);
-            let value = -self.search(board, depth, -beta, -alpha);
+            let value = -self.search(board, Self::DEPTH, LOW_SCORE, -alpha);
             board.unmake_move(mov);
 
-            if value > best_value {
+            if value > alpha {
+                alpha = value;
                 best_moves = vec![mov];
-                best_value = value;
-
-                alpha = i32::max(alpha, best_value);
-
-                if alpha >= beta {
-                    break;
-                }
-            } else if value == best_value {
+            } else if value == alpha {
                 best_moves.push(mov);
             }
         }
 
         let best_move = best_moves.choose(&mut rand::thread_rng()).cloned();
 
-        (best_move, best_value)
+        (best_move, alpha)
     }
 }
 
@@ -152,8 +142,8 @@ impl AlphaBetaSearcher for QuiescenceSearcher {
     }
 
     fn should_terminate(board: &mut Board) -> bool {
-        let mut min = std::i32::MAX;
-        let mut max = std::i32::MIN;
+        let mut min = HIGH_SCORE;
+        let mut max = LOW_SCORE;
 
         for mov in board.moves() {
             board.make_move(mov);
