aa5758f2670afc2a237fb13e835ab06aed4ffcc1
Felix Chapman <aelred717@gmail.com>
2018-12-22 15:58:51 +0000

    Add helper threads during search

--- src/search.rs
+++ src/search.rs
@@ -1,9 +1,14 @@
 use crate::Board;
 use crate::Move;
 use crate::Player;
+use chashmap::CHashMap;
 use enum_map::EnumMap;
 use rand::seq::SliceRandom;
-use std::collections::HashMap;
+use rand::thread_rng;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
+use std::sync::Arc;
+use std::thread;
 
 const HIGH_SCORE: i32 = std::i32::MAX;
 const LOW_SCORE: i32 = -HIGH_SCORE;
@@ -12,6 +17,8 @@ type Key = (
     EnumMap<crate::Player, EnumMap<crate::PieceType, crate::Bitboard>>,
     Player,
 );
+
+type TranspositionTable = Arc<CHashMap<Key, TranspositionEntry>>;
 
 struct TranspositionEntry {
     depth: u32,
@@ -25,34 +32,231 @@ enum Flag {
     UpperBound,
 }
 
-#[derive(Default)]
-pub struct Searcher {
-    transposition_table: HashMap<Key, TranspositionEntry>,
-}
-
 impl Board {
     fn key(&self) -> Key {
         (*self.bitboards(), self.player())
     }
 }
 
-impl Searcher {
-    const DEPTH: u32 = 4;
+#[derive(Debug)]
+enum Message {
+    StartSearch(Box<Board>),
+    AbortSearch,
+    AbortThread,
+}
 
+pub struct Searcher {
+    txs: Vec<Sender<Message>>,
+    transposition_table: TranspositionTable,
+}
+
+const NUM_THREADS: u32 = 4;
+
+impl Default for Searcher {
+    fn default() -> Self {
+        let transposition_table = TranspositionTable::default();
+        let mut txs = vec![];
+
+        for _ in 0..NUM_THREADS {
+            let transposition_table = transposition_table.clone();
+            let (tx, rx) = std::sync::mpsc::channel();
+
+            thread::spawn(move || worker_thread(&transposition_table, &rx));
+
+            txs.push(tx);
+        }
+
+        Self {
+            txs,
+            transposition_table,
+        }
+    }
+}
+
+fn worker_thread(transposition_table: &TranspositionTable, rx: &Receiver<Message>) {
+    loop {
+        match rx.recv().unwrap() {
+            Message::StartSearch(mut board) => {
+                let mut searcher = ThreadSearcher::new(&mut board, transposition_table, rx);
+                searcher.run();
+            }
+            Message::AbortThread => {
+                return;
+            }
+            Message::AbortSearch => {
+                // We're already done searching, so ignore
+            }
+        }
+    }
+}
+
+impl Drop for Searcher {
+    fn drop(&mut self) {
+        for tx in &self.txs {
+            let _ = tx.send(Message::AbortThread);
+        }
+    }
+}
+
+impl Searcher {
     pub fn run(&mut self, board: &mut Board) -> (Option<Move>, i32) {
-        let mut moves = board.pseudo_legal_moves().peekable();
+        for tx in &self.txs {
+            tx.send(Message::StartSearch(Box::new(board.clone())))
+                .unwrap();
+        }
+
+        let transposition_table = self.transposition_table.clone();
+        let mut local_searcher = LocalSearcher::new(board, transposition_table);
+
+        let result = local_searcher.run();
+
+        for tx in &self.txs {
+            tx.send(Message::AbortSearch).unwrap();
+        }
+
+        result
+    }
+}
+
+const DEPTH: u32 = 5;
+
+struct ThreadSearcher<'a> {
+    board: &'a mut Board,
+    transposition_table: &'a TranspositionTable,
+    rx: &'a Receiver<Message>,
+}
+
+impl<'a> ThreadSearcher<'a> {
+    fn new(
+        board: &'a mut Board,
+        transposition_table: &'a TranspositionTable,
+        rx: &'a Receiver<Message>,
+    ) -> Self {
+        Self {
+            board,
+            transposition_table,
+            rx,
+        }
+    }
+
+    fn run(&mut self) {
+        let mut moves: Vec<Move> = self.board.pseudo_legal_moves().collect();
+        moves.shuffle(&mut thread_rng());
+
+        let mut alpha = LOW_SCORE;
+
+        for mov in moves {
+            self.board.make_move(mov);
+            if let Some(value) = self.search(DEPTH - 1, LOW_SCORE, -alpha) {
+                let value = -value;
+
+                if value > alpha {
+                    alpha = value;
+                }
+            } else {
+                return;
+            }
+            self.board.unmake_move(mov);
+        }
+    }
+
+    fn search(&mut self, depth: u32, mut alpha: i32, mut beta: i32) -> Option<i32> {
+        let alpha_orig = alpha;
+
+        let key = self.board.key();
+
+        if let Some(entry) = self.transposition_table.get(&key) {
+            if entry.depth >= depth {
+                match entry.flag {
+                    Flag::Exact => {
+                        return Some(entry.value);
+                    }
+                    Flag::LowerBound => {
+                        alpha = i32::max(alpha, entry.value);
+                    }
+                    Flag::UpperBound => {
+                        beta = i32::min(beta, entry.value);
+                    }
+                }
+
+                if alpha >= beta {
+                    return Some(entry.value);
+                }
+            }
+        }
+
+        let value = self.search_uncached(depth, alpha, beta)?;
+
+        let flag = if value <= alpha_orig {
+            Flag::UpperBound
+        } else if value >= beta {
+            Flag::LowerBound
+        } else {
+            Flag::Exact
+        };
+
+        let entry = TranspositionEntry { depth, value, flag };
+
+        self.transposition_table.insert(key, entry);
+
+        Some(value)
+    }
+
+    fn search_uncached(&mut self, depth: u32, mut alpha: i32, beta: i32) -> Option<i32> {
+        if depth == 0 {
+            if let Ok(Message::AbortSearch) = self.rx.try_recv() {
+                return None;
+            }
+
+            return Some(quiesce(&mut self.board, alpha, beta));
+        }
+
+        let mut moves = self.board.pseudo_legal_moves().peekable();
 
         if moves.peek().is_none() {
-            return (None, LOW_SCORE);
+            return Some(LOW_SCORE);
         }
+
+        for mov in moves {
+            self.board.make_move(mov);
+            let value = -self.search(depth - 1, -beta, -alpha)?;
+            self.board.unmake_move(mov);
+
+            if value >= beta {
+                return Some(beta);
+            }
+            if value > alpha {
+                alpha = value;
+            }
+        }
+
+        Some(alpha)
+    }
+}
+
+struct LocalSearcher<'a> {
+    board: &'a mut Board,
+    transposition_table: TranspositionTable,
+}
+
+impl<'a> LocalSearcher<'a> {
+    fn new(board: &'a mut Board, transposition_table: TranspositionTable) -> Self {
+        Self {
+            board,
+            transposition_table,
+        }
+    }
+
+    fn run(&mut self) -> (Option<Move>, i32) {
+        let moves = self.board.pseudo_legal_moves();
 
         let mut alpha = LOW_SCORE;
         let mut best_moves = vec![];
 
         for mov in moves {
-            board.make_move(mov);
-            let value = -self.search(board, Self::DEPTH, LOW_SCORE, -alpha);
-            board.unmake_move(mov);
+            self.board.make_move(mov);
+            let value = -self.search(DEPTH, LOW_SCORE, -alpha);
+            self.board.unmake_move(mov);
 
             if value > alpha {
                 alpha = value;
@@ -67,10 +271,10 @@ impl Searcher {
         (best_move, alpha)
     }
 
-    fn search(&mut self, board: &mut Board, depth: u32, mut alpha: i32, mut beta: i32) -> i32 {
+    fn search(&mut self, depth: u32, mut alpha: i32, mut beta: i32) -> i32 {
         let alpha_orig = alpha;
 
-        let key = board.key();
+        let key = self.board.key();
 
         if let Some(entry) = self.transposition_table.get(&key) {
             if entry.depth >= depth {
@@ -92,7 +296,7 @@ impl Searcher {
             }
         }
 
-        let value = self.search_uncached(board, depth, alpha, beta);
+        let value = self.search_uncached(depth, alpha, beta);
 
         let flag = if value <= alpha_orig {
             Flag::UpperBound
@@ -109,21 +313,21 @@ impl Searcher {
         value
     }
 
-    fn search_uncached(&mut self, board: &mut Board, depth: u32, mut alpha: i32, beta: i32) -> i32 {
+    fn search_uncached(&mut self, depth: u32, mut alpha: i32, beta: i32) -> i32 {
         if depth == 0 {
-            return Self::quiesce(board, alpha, beta);
+            return quiesce(&mut self.board, alpha, beta);
         }
 
-        let mut moves = board.pseudo_legal_moves().peekable();
+        let mut moves = self.board.pseudo_legal_moves().peekable();
 
         if moves.peek().is_none() {
             return LOW_SCORE;
         }
 
         for mov in moves {
-            board.make_move(mov);
-            let value = -self.search(board, depth - 1, -beta, -alpha);
-            board.unmake_move(mov);
+            self.board.make_move(mov);
+            let value = -self.search(depth - 1, -beta, -alpha);
+            self.board.unmake_move(mov);
 
             if value >= beta {
                 return beta;
@@ -135,31 +339,31 @@ impl Searcher {
 
         alpha
     }
+}
 
-    fn quiesce(board: &mut Board, mut alpha: i32, beta: i32) -> i32 {
-        let stand_pat = board.eval();
+fn quiesce(board: &mut Board, mut alpha: i32, beta: i32) -> i32 {
+    let stand_pat = board.eval();
 
-        if stand_pat >= beta {
+    if stand_pat >= beta {
+        return beta;
+    }
+
+    if alpha < stand_pat {
+        alpha = stand_pat;
+    }
+
+    for mov in board.capturing_moves() {
+        board.make_move(mov);
+        let value = -quiesce(board, -beta, -alpha);
+        board.unmake_move(mov);
+
+        if value >= beta {
             return beta;
         }
-
-        if alpha < stand_pat {
-            alpha = stand_pat;
+        if value > alpha {
+            alpha = value;
         }
-
-        for mov in board.capturing_moves() {
-            board.make_move(mov);
-            let value = -Self::quiesce(board, -beta, -alpha);
-            board.unmake_move(mov);
-
-            if value >= beta {
-                return beta;
-            }
-            if value > alpha {
-                alpha = value;
-            }
-        }
-
-        alpha
     }
+
+    alpha
 }
