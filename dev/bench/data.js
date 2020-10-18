498c30f3c18cfec6ca0f5616b48519f443d2f468
github-action-benchmark <github@users.noreply.github.com>
2020-10-18 12:24:34 +0000

    add Benchmark (cargo) benchmark result for 940b2e89bf2b2f4cce6c7e597b5d1e0bcb18a989

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1603021810692,
+  "lastUpdate": 1603023874675,
   "repoUrl": "https://github.com/aelred/skakoui",
   "entries": {
     "Benchmark": [
@@ -71,6 +71,78 @@ window.BENCHMARK_DATA = {
             "name": "mate/in3",
             "value": 2188231217,
             "range": "± 62203244",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aelred717@gmail.com",
+            "name": "Felix Chapman",
+            "username": "aelred"
+          },
+          "committer": {
+            "email": "aelred717@gmail.com",
+            "name": "Felix Chapman",
+            "username": "aelred"
+          },
+          "distinct": true,
+          "id": "940b2e89bf2b2f4cce6c7e597b5d1e0bcb18a989",
+          "message": "Change how we specify benchmarks",
+          "timestamp": "2020-10-18T13:19:42+01:00",
+          "tree_id": "2ea16ed38c151a8e356ad7d3adcb8001e3e3c525",
+          "url": "https://github.com/aelred/skakoui/commit/940b2e89bf2b2f4cce6c7e597b5d1e0bcb18a989"
+        },
+        "date": 1603023871725,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "perft_init/1",
+            "value": 4446,
+            "range": "± 5",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perft_init/2",
+            "value": 96847,
+            "range": "± 7837",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perft_init/3",
+            "value": 2038703,
+            "range": "± 1028",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perft_init/4",
+            "value": 45505990,
+            "range": "± 1300584",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "perft_kiwipete/1",
+            "value": 9776,
+            "range": "± 507",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "mate/in1",
+            "value": 140268041,
+            "range": "± 5463882",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "mate/in2",
+            "value": 2040256033,
+            "range": "± 32224350",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "mate/in3",
+            "value": 3019509389,
+            "range": "± 78739640",
             "unit": "ns/iter"
           }
         ]
